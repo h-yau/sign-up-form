5f4978cc94ac3c30e550484bcf85c658d5f8ad1b
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2023-08-16 11:11:00 -0700

    Implement simple password check

--- sign-up-form.js
+++ sign-up-form.js
@@ -1,5 +1,16 @@
 const form = document.querySelector('form');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+
+
+
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(e.target)
-})
+    if (password.value == confirmPassword.value) {
+        console.log('Success');
+    } else {
+        console.log('No Match');
+    }
+});
+
+
