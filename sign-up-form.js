e6ce57901f016c3e945682064be6db08b21d2967
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2023-08-16 11:25:14 -0700

    Create custom error message for non-match passwords

--- sign-up-form.js
+++ sign-up-form.js
@@ -3,6 +3,14 @@ const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirm-password');
 
 
+confirmPassword.addEventListener("input", (e) => {
+    if (confirmPassword.value != password.value) {
+        confirmPassword.setCustomValidity("Passwords should match!");
+        
+    }
+})
+
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
