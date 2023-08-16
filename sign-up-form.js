c3a3728da5528e064e77ea007cdeaec65dc044ee
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2023-08-16 12:37:38 -0700

    Improve logic on and beautify  valid and invalid inputs

--- sign-up-form.js
+++ sign-up-form.js
@@ -1,15 +1,31 @@
 const form = document.querySelector('form');
+const inputs = document.querySelectorAll('input');
 const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirm-password');
 
 
+/// still working on the input green/red while not focus situation
+inputs.forEach(input => {
+    input.addEventListener("input", event => {
+        input = event.target
+        if (input.validity.valid) {
+            input.classList.add('valid');
+            input.classList.remove('invalid');
+        } else {
+            input.classList.add('invalid');
+            input.classList.remove('valid');
+        }
+    });
+});
+
+
+
 confirmPassword.addEventListener("input", (e) => {
+    e.preventDefault();
     if (confirmPassword.value != password.value) {
         confirmPassword.setCustomValidity("Passwords should match!");
     }
 })
-
-
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
